381005ad68764aae285ef0d540817507801b2dfe
aniketbhujbal55 <aniketbhujbal55@gmail.com>
2022-12-10 12:29:07 -0800

    shree

---
+++ loop1.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class loop1 {
+
+	public static void main(String[] args) {
+	// wap for 1 to 5 no by using for loop
+		
+		for (int i=1; i<=5; i++) {
+			System.out.println("hello:" );
+		}
+		
+
+	}
+
+}
